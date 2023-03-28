b16f23eb920e1f87f8589fb6d32beb9f91723969
Andrew-cx8 <124941213+Andrew-cx8@users.noreply.github.com>
2023-03-27 19:02:55 -0500

    Proyecto base con división

---
+++ Division.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperacionDividir
+{
+    public class Division
+    {
+        public static float dividir(float a, float b)
+        {
+            return a / b;
+        }
+    }
+}
